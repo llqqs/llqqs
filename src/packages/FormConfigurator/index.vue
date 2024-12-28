d0fdad284c7e00348eec1c3cd92d51461c452fe0
格子 <fl9420@qq.com>
2024-12-28 23:36:39 +0800

    chore: basic config & element-plus

--- src/packages/FormConfigurator/index.vue
+++ src/packages/FormConfigurator/index.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-
+defineOptions({
+  name: 'FormConfigurator',
+})
 </script>
 
 <template>
-
+  <el-form>
+    <el-form-item label="姓名">
+      <el-input></el-input>
+    </el-form-item>
+    <el-form-item label="性别">
+      <el-input></el-input>
+    </el-form-item>
+    <el-form-item label="年龄">
+      <el-input></el-input>
+    </el-form-item>
+  </el-form>
 </template>
 
 <style scoped>
