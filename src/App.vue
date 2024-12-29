7fc4eac934b81d88bb55b2f68435c2e465b423c6
格子 <fl9420@qq.com>
2024-12-29 21:04:45 +0800

    feat(components): 定义表单类型

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,182 @@
 <script setup lang="ts">
-import FormConfigurator from '@packages/FormConfigurator/index.vue'
+import FormConfigurator from '@packages/FormConfigurator/src/index.vue'
+import {reactive} from "vue";
+import {type FormConfig, FormItemType} from "@packages/FormConfigurator/type";
+import {cloneDeep} from 'lodash'
+
+const formConfig = reactive<FormConfig>({
+  formItems: [
+    {
+      type: FormItemType.RADIO,
+      label: '性别',
+      prop: 'gender',
+      attribute: {
+        options: [
+          {
+            value: 1,
+            label: '男',
+          },
+          {
+            value: 2,
+            label: '女',
+          }
+        ],
+      }
+    },
+    {
+      type: FormItemType.CHECKBOX,
+      label: '爱好',
+      prop: 'hobby',
+      attribute: {
+        options: [
+          {
+            value: 1,
+            label: '语文',
+          },
+          {
+            value: 2,
+            label: '数学',
+          },
+          {
+            value: 3,
+            label: '物理',
+          }
+        ],
+      }
+    },
+    {
+      type: FormItemType.INPUT,
+      label: '姓名',
+      prop: 'name',
+    },
+    {
+      type: FormItemType.INPUT_NUMBER,
+      label: '年龄',
+      prop: 'age',
+    },
+    {
+      type: FormItemType.SELECT,
+      label: '政治面貌',
+      prop: 'politicalOutlook',
+      attribute: {
+        options: [
+          {
+            value: 1,
+            label: '党员',
+          },
+          {
+            value: 2,
+            label: '团员',
+          }
+        ],
+      }
+    },
+    {
+      type: FormItemType.CASCADER,
+      label: '地址',
+      prop: 'address',
+      attribute: {
+        options: [
+          {
+            value: '001',
+            label: '北京市',
+            children: [
+              {
+                value: '00101',
+                label: '朝阳区',
+              },
+              {
+                value: '00102',
+                label: '海淀区',
+              }
+            ]
+          },
+          {
+            value: '002',
+            label: '重庆市',
+            children: [
+              {
+                value: '00201',
+                label: '渝北区',
+              },
+              {
+                value: '00202',
+                label: '江北区',
+              },
+              {
+                value: '00203',
+                label: '南岸区',
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      type: FormItemType.SWITCH,
+      label: '是否通过',
+      prop: 'whetherPass',
+    },
+    {
+      type: FormItemType.SLIDER,
+      label: '意愿度',
+      prop: 'willingness',
+    },
+    {
+      type: FormItemType.TIME_PICKER,
+      label: '起床时间',
+      prop: 'wakeUpTime',
+    },
+    {
+      type: FormItemType.DATE_PICKER,
+      label: '出生年月',
+      prop: 'birthday',
+    },
+    {
+      type: FormItemType.UPLOAD,
+      label: '头像',
+      prop: 'avatar',
+    },
+    {
+      type: FormItemType.RATE,
+      label: '评价',
+      prop: 'rate',
+    },
+    {
+      type: FormItemType.COLOR_PICKER,
+      label: '喜欢的颜色',
+      prop: 'color',
+    },
+  ]
+})
+
+const modelModel = reactive({
+  name: '格子惊蛰版'
+})
+
+const formConfigReadonly = reactive<FormConfig>({
+  ...cloneDeep(formConfig),
+  attribute: {
+    disabled: true,
+  }
+})
 </script>
 
 <template>
-  <FormConfigurator/>
+  <el-row :gutter="30">
+    <el-col :span="12">
+      <FormConfigurator
+          :model="modelModel"
+          :config="formConfig"
+      />
+    </el-col>
+    <el-col :span="12">
+      <FormConfigurator
+          :model="modelModel"
+          :config="formConfigReadonly"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <style scoped>
---
+++ src/packages/FormConfigurator/src/index.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import {type FormConfig, FormItemType} from "@packages/FormConfigurator/type";
+
+defineProps<{
+  config: FormConfig,
+  model: any
+}>()
+defineOptions({
+  name: 'FormConfigurator',
+})
+
+</script>
+
+<template>
+  <el-form :model="model" v-bind="config.attribute">
+    <el-row>
+      <template v-for="item in config.formItems" :key="item.prop">
+        <el-col v-if="FormItemType.RADIO == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-radio-group v-model="model[item.prop]">
+              <el-radio v-for="option in item.attribute?.options" :key="option.value" :label="option.value">
+                {{ option.label }}
+              </el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.CHECKBOX == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-checkbox-group v-model="model[item.prop]">
+              <el-checkbox v-for="option in item.attribute?.options" :key="option.value" :label="option.value">
+                {{ option.label }}
+              </el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.INPUT == item.type">
+          <el-form-item
+              :label="item.label" :prop="item.prop">
+            <el-input
+                v-model="model[item.prop]">
+            </el-input>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.INPUT_NUMBER == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-input-number
+                v-model="model[item.prop]">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.SELECT == item.type">
+          <el-form-item
+              :label="item.label" :prop="item.prop"
+          >
+            <el-select
+                v-model="model[item.prop]"
+            >
+              <el-option v-for="option in item.attribute?.options" :key="option.value" :label="option.label"
+                         :value="option.value"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.CASCADER == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-cascader
+                v-model="model[item.prop]"
+                :options="item.attribute?.options">
+            </el-cascader>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.SWITCH == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-switch
+                v-model="model[item.prop]">
+            </el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.SLIDER == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-slider
+                v-model="model[item.prop]">
+            </el-slider>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.TIME_PICKER == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-time-select
+                v-model="model[item.prop]">
+            </el-time-select>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.DATE_PICKER == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-date-picker
+                type="date"
+                v-model="model[item.prop]">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.UPLOAD == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-upload>
+            </el-upload>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.RATE == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-rate v-model="model[item.prop]"></el-rate>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="FormItemType.COLOR_PICKER == item.type">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-color-picker v-model="model[item.prop]"></el-color-picker>
+          </el-form-item>
+        </el-col>
+      </template>
+    </el-row>
+  </el-form>
+</template>
